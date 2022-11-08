34c22adbb7824cfe8255c75e8975774f0e8efc29
Jane <e.u.bugaeva@yandex.ru>
2022-11-08 14:31:09 +0300

    End of testing 6

--- DZ_6/Program.cs
+++ DZ_6/Program.cs
@@ -8,23 +8,17 @@
 void PositiveMethod()
 {
     Console.Write("Введите количество чисел: ");
-
     int size = Convert.ToInt32(Console.ReadLine());
-
     int count = 0;
-
     for (int i = 0; i < size; i++)
     {
         Console.Write($"Введите значение {i+1}: ");
-
         int num = Convert.ToInt32(Console.ReadLine());
-
         if (num > 0)
         {
             count++;
         }
     }
-
 Console.WriteLine($"-> {count}");
 }
 
